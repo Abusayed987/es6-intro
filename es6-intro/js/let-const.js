b201f6a0d5912301aee275a4539e06df5c0fa123
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-10-06 10:36:23 +0600

    added template dtring

---
+++ es6-intro/js/let-const.js
@@ -0,0 +1,19 @@
+/** var
+ * var variables can be re-declared and updated
+ * Hoisting of var
+Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution.
+ *
+ */
+
+
+/** let
+ * let is block scoped
+ * let can be updated but not re-declared.
+*Hoisting of let
+Just like  var, let declarations are hoisted to the top. Unlike var which is initialized as undefined, the let keyword is not initialized. So if you try to use a let variable before declaration, you'll get a Reference Error.
+ */
+
+
+/** Const
+ * const cannot be updated or re-declared
+ */
---
+++ es6-intro/js/template-string.js
@@ -0,0 +1,9 @@
+// Practice:01
+const name = `Md. Abu Sayed ` ;
+const age = 21;
+// console.log(`my name is ${name} & i am ${age} Years old!, This is a template string`);
+
+// practice:02
+const friendsName = `hablu , bablu , kablu ,harun mia`;
+console.log(`ata sobai holo friends kara ata ,are ara kara , ata holo${friendsName}`);
+
