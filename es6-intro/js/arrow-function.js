33b3767865947be43e4f690690c832ac7724951d
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-10-06 15:08:04 +0600

    added arrow function

---
+++ es6-intro/js/arrow-function.js
@@ -0,0 +1,18 @@
+// practice:01--------------------------------------------
+const add1 = function (num1, num2) {
+    const total = num1 + num2;
+    return total;
+}
+const result = add1(2, 1);
+// console.log(result);
+
+const add2 = (num1, num2) => num1 + num2;
+// console.log(add2(12,23));
+
+
+// practice:02----------------------------------------------
+const add3 = (num1, num2) => num1 + num2;
+console.log(add3(23, 45));
+//practice:03------------------------------------------------
+const sum = (num1,num2) => num1+ num2;
+console.log(sum(54321,5432));
